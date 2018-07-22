560f4ff17b31c7acf1be8a2228bfb6344437f3a1
William da Silva Oliveira <1704639@aluno.univesp.br>
2018-07-22 18:01:42 -0300

    Add changes to the code
    Add refresh page, remove element when width is under 990px if the page get above this display a message.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,38 @@
-var hero1 = 'img/hero/america.png'; 
-var hero2 = 'img/hero/arqueiro.jpg'; 
-var hero3 = 'img/hero/capMarvel.jpg'; 
-var hero4 = 'img/hero/coisa.jpg'; 
-var hero5 = 'img/hero/demolidor.jpg'; 
-var hero6 = 'img/hero/estranho.jpg'; 
-var hero7 = 'img/hero/falcao.jpg'; 
-var hero8 = 'img/hero/feiticeira.jpg'; 
-var hero9 = 'img/hero/hulk.jpg'; 
-var hero10 = 'img/hero/invisivel.jpg'; 
-var hero11 = 'img/hero/ironMan.jpg'; 
-var hero12 = 'img/hero/motoqueiro.jpg'; 
-var hero13 = 'img/hero/pantera.jpg'; 
-var hero14 = 'img/hero/fantastico.jpg'; 
-var hero15 = 'img/hero/spidey.jpg'; 
-var hero16 = 'img/hero/surfer.jpg'; 
-var hero17 = 'img/hero/thor.jpg'; 
+var hero1 = 'img/hero/america.png';
+var hero2 = 'img/hero/arqueiro.jpg';
+var hero3 = 'img/hero/capMarvel.jpg';
+var hero4 = 'img/hero/coisa.jpg';
+var hero5 = 'img/hero/demolidor.jpg';
+var hero6 = 'img/hero/estranho.jpg';
+var hero7 = 'img/hero/falcao.jpg';
+var hero8 = 'img/hero/feiticeira.jpg';
+var hero9 = 'img/hero/hulk.jpg';
+var hero10 = 'img/hero/invisivel.jpg';
+var hero11 = 'img/hero/ironMan.jpg';
+var hero12 = 'img/hero/motoqueiro.jpg';
+var hero13 = 'img/hero/pantera.jpg';
+var hero14 = 'img/hero/fantastico.jpg';
+var hero15 = 'img/hero/spidey.jpg';
+var hero16 = 'img/hero/surfer.jpg';
+var hero17 = 'img/hero/thor.jpg';
 var hero18 = 'img/hero/tocha.jpg';
 
 var heros = [hero1, hero2, hero3, hero4, hero5, hero6, hero7, hero8, hero9, hero10, hero11, hero12, hero13, hero14, hero15, hero16, hero17, hero18];
 
+function refresh() {
+    $('#re').addClass('fa-spin');
 
-function sm () {
-    location.reload(); 
-    alert(hero1);
+}
+
+
+function refresh() {
+    location.reload();
 }
 
 
 if (window.innerWidth < 992) {
-    
-            
+
+
     $("#5").remove();
     $("#6").remove();
     $("#11").remove();
@@ -37,307 +41,310 @@ if (window.innerWidth < 992) {
     $("#18").remove();
     $("#23").remove();
     $("#24").remove();
-    var y = 2;  
-        
-
-function card1()  {
-
-$('#card1').addClass('animated zoomIn');  
-    
-document.getElementById("card1").src = heros[0];
-  
-}
-
-function card2() {
-
-$('#card2').addClass('animated zoomIn');  
-    
-document.getElementById("card2").src = heros[1];    
-}
-
-function card3() {
-
-$('#card3').addClass('animated zoomIn');  
-    
-document.getElementById("card3").src = heros[2];    
-}
-
-function card4() {
-
-$('#card4').addClass('animated zoomIn');  
-    
-document.getElementById("card4").src = heros[3];}    
-    
-function card7() {
-$('#card7').addClass('animated zoomIn');  
-    
-document.getElementById("card7").src = heros[1];    
-}
-
-function card8() {
-
-$('#card8').addClass('animated zoomIn');  
-    
-document.getElementById("card8").src = heros[5];    
-}
-
-function card9() {
-
-$('#card9').addClass('animated zoomIn');  
-    
-document.getElementById("card9").src = heros[6];    
-}
-
-function card10() {
-
-$('#card10').addClass('animated zoomIn');  
-    
-document.getElementById("card10").src = heros[7];    
-}
-
-function card13() {
-
-$('#card13').addClass('animated zoomIn');  
-    
-document.getElementById("card13").src = heros[8];    
-}
-
-function card14() {
-
-$('#card14').addClass('animated zoomIn');  
-    
-document.getElementById("card14").src = heros[9];    
-}
+    var y = 2;
 
 
-function card15() {
-$('#card15').addClass('animated zoomIn');  
-    
-document.getElementById("card15").src = heros[10];    
-}
+    function card1() {
+
+        $('#card1').addClass('animated zoomIn');
+
+        document.getElementById("card1").src = heros[0];
+
+    }
+
+    function card2() {
+
+        $('#card2').addClass('animated zoomIn');
+
+        document.getElementById("card2").src = heros[1];
+    }
+
+    function card3() {
+
+        $('#card3').addClass('animated zoomIn');
+
+        document.getElementById("card3").src = heros[2];
+    }
+
+    function card4() {
+
+        $('#card4').addClass('animated zoomIn');
+
+        document.getElementById("card4").src = heros[3];
+    }
+
+    function card7() {
+        $('#card7').addClass('animated zoomIn');
+
+        document.getElementById("card7").src = heros[1];
+    }
+
+    function card8() {
+
+        $('#card8').addClass('animated zoomIn');
+
+        document.getElementById("card8").src = heros[5];
+    }
+
+    function card9() {
+
+        $('#card9').addClass('animated zoomIn');
+
+        document.getElementById("card9").src = heros[6];
+    }
+
+    function card10() {
+
+        $('#card10').addClass('animated zoomIn');
+
+        document.getElementById("card10").src = heros[7];
+    }
+
+    function card13() {
+
+        $('#card13').addClass('animated zoomIn');
+
+        document.getElementById("card13").src = heros[8];
+    }
+
+    function card14() {
+
+        $('#card14').addClass('animated zoomIn');
+
+        document.getElementById("card14").src = heros[9];
+    }
 
 
-function card16() {
-$('#card16').addClass('animated zoomIn');  
-    
-document.getElementById("card16").src = heros[11];    
-}
+    function card15() {
+        $('#card15').addClass('animated zoomIn');
+
+        document.getElementById("card15").src = heros[10];
+    }
 
 
-function card17() {
-$('#card17').addClass('animated zoomIn');  
-    
-document.getElementById("card17").src = heros[12];    
-}
+    function card16() {
+        $('#card16').addClass('animated zoomIn');
 
-function card19() {
+        document.getElementById("card16").src = heros[11];
+    }
 
-$('#card19').addClass('animated zoomIn');  
-    
-document.getElementById("card19").src = heros[13];    
-}
 
-function card20() {
+    function card17() {
+        $('#card17').addClass('animated zoomIn');
 
-$('#card20').addClass('animated zoomIn');  
-    
-document.getElementById("card20").src = heros[14];    
-}
+        document.getElementById("card17").src = heros[12];
+    }
 
-function card21() {
-$('#card21').addClass('animated zoomIn');  
-    
-document.getElementById("card21").src = heros[15];    
-}
+    function card19() {
 
-function card22() {
-$('#card22').addClass('animated zoomIn');  
-    
-document.getElementById("card22").src = heros[16];    
-}    
+        $('#card19').addClass('animated zoomIn');
 
-}
+        document.getElementById("card19").src = heros[13];
+    }
 
-           
-if( window.innerWidth > 992)   {
-    
-function card1()  {
+    function card20() {
 
-$('#card1').addClass('animated zoomIn');  
-    
-document.getElementById("card1").src = heros[0];    
+        $('#card20').addClass('animated zoomIn');
+
+        document.getElementById("card20").src = heros[14];
+    }
+
+    function card21() {
+        $('#card21').addClass('animated zoomIn');
+
+        document.getElementById("card21").src = heros[15];
+    }
+
+    function card22() {
+        $('#card22').addClass('animated zoomIn');
+
+        document.getElementById("card22").src = heros[16];
+    }
+
 }
 
 
-function card2() {
+if (window.innerWidth > 992) {
 
-$('#card2').addClass('animated zoomIn');  
-    
-document.getElementById("card2").src = heros[1];    
+    function card1() {
+
+        $('#card1').addClass('animated zoomIn');
+
+        document.getElementById("card1").src = heros[0];
+    }
+
+
+    function card2() {
+
+        $('#card2').addClass('animated zoomIn');
+
+        document.getElementById("card2").src = heros[1];
+    }
+
+
+    function card3() {
+
+        $('#card3').addClass('animated zoomIn');
+
+        document.getElementById("card3").src = heros[2];
+    }
+
+
+    function card4() {
+
+        $('#card4').addClass('animated zoomIn');
+
+        document.getElementById("card4").src = heros[3];
+    }
+
+
+    function card5() {
+
+        $('#card5').addClass('animated zoomIn');
+
+        document.getElementById("card5").src = heros[4];
+    }
+
+    function card6() {
+
+        $('#card6').addClass('animated zoomIn');
+
+        document.getElementById("card6").src = heros[5];
+    }
+
+    function card7() {
+        $('#card7').addClass('animated zoomIn');
+
+        document.getElementById("card7").src = heros[6];
+    }
+
+    function card8() {
+
+        $('#card8').addClass('animated zoomIn');
+
+        document.getElementById("card8").src = heros[7];
+    }
+
+    function card9() {
+
+        $('#card9').addClass('animated zoomIn');
+
+        document.getElementById("card9").src = heros[8];
+    }
+
+    function card10() {
+
+        $('#card10').addClass('animated zoomIn');
+
+        document.getElementById("card10").src = heros[9];
+    }
+
+    function card11() {
+
+        $('#card11').addClass('animated zoomIn');
+
+        document.getElementById("card11").src = heros[10];
+    }
+
+
+    function card12() {
+        $('#card12').addClass('animated zoomIn');
+
+        document.getElementById("card12").src = heros[11];
+    }
+
+    function card13() {
+
+        $('#card13').addClass('animated zoomIn');
+
+        document.getElementById("card13").src = heros[12];
+    }
+
+    function card14() {
+
+        $('#card14').addClass('animated zoomIn');
+
+        document.getElementById("card14").src = heros[13];
+    }
+
+
+    function card15() {
+        $('#card15').addClass('animated zoomIn');
+
+        document.getElementById("card15").src = heros[14];
+    }
+
+
+    function card16() {
+        $('#card16').addClass('animated zoomIn');
+
+        document.getElementById("card16").src = heros[15];
+    }
+
+
+    function card17() {
+        $('#card17').addClass('animated zoomIn');
+
+        document.getElementById("card17").src = heros[16];
+    }
+
+    function card18() {
+
+        $('#card18').addClass('animated zoomIn');
+
+        document.getElementById("card18").src = heros[17];
+    }
+
+    function card19() {
+
+        $('#card19').addClass('animated zoomIn');
+
+        document.getElementById("card19").src = heros[18];
+    }
+
+    function card20() {
+
+        $('#card20').addClass('animated zoomIn');
+
+        document.getElementById("card20").src = heros[19];
+    }
+
+    function card21() {
+        $('#card21').addClass('animated zoomIn');
+
+        document.getElementById("card21").src = heros[20];
+    }
+
+    function card22() {
+        $('#card22').addClass('animated zoomIn');
+
+        document.getElementById("card22").src = heros[21];
+    }
+
+    function card23() {
+        $('#card23').addClass('animated zoomIn');
+
+        document.getElementById("card23").src = heros[22];
+    }
+
+
+    function card24() {
+        $('#card24').addClass('animated zoomIn');
+
+        document.getElementById("card24").src = heros[23];
+    }
+
+
 }
 
 
-function card3() {
-
-$('#card3').addClass('animated zoomIn');  
-    
-document.getElementById("card3").src = heros[2];    
-}
-
-
-function card4() {
-
-$('#card4').addClass('animated zoomIn');  
-    
-document.getElementById("card4").src = heros[3];    
-}
-
-
-function card5() {
-
-$('#card5').addClass('animated zoomIn');  
-    
-document.getElementById("card5").src = heros[4]; 
-}
-
-function card6() {
-
-$('#card6').addClass('animated zoomIn');  
-    
-document.getElementById("card6").src = heros[5];    
-}
-
-function card7() {
-$('#card7').addClass('animated zoomIn');  
-    
-document.getElementById("card7").src = heros[6];    
-}
-
-function card8() {
-
-$('#card8').addClass('animated zoomIn');  
-    
-document.getElementById("card8").src = heros[7];    
-}
-
-function card9() {
-
-$('#card9').addClass('animated zoomIn');  
-    
-document.getElementById("card9").src = heros[8];    
-}
-
-function card10() {
-
-$('#card10').addClass('animated zoomIn');  
-    
-document.getElementById("card10").src = heros[9];    
-}
-
-function card11() {
-
-$('#card11').addClass('animated zoomIn');  
-    
-document.getElementById("card11").src = heros[10];    
-}
-
-
-function card12() {
-$('#card12').addClass('animated zoomIn');  
-    
-document.getElementById("card12").src = heros[11];    
-}
-
-function card13() {
-
-$('#card13').addClass('animated zoomIn');  
-    
-document.getElementById("card13").src = heros[12];    
-}
-
-function card14() {
-
-$('#card14').addClass('animated zoomIn');  
-    
-document.getElementById("card14").src = heros[13];    
-}
-
-
-function card15() {
-$('#card15').addClass('animated zoomIn');  
-    
-document.getElementById("card15").src = heros[14];    
-}
-
-
-function card16() {
-$('#card16').addClass('animated zoomIn');  
-    
-document.getElementById("card16").src = heros[15];    
-}
-
-
-function card17() {
-$('#card17').addClass('animated zoomIn');  
-    
-document.getElementById("card17").src = heros[16];    
-}
-
-function card18() {
-
-$('#card18').addClass('animated zoomIn');  
-    
-document.getElementById("card18").src = heros[17];    
-}
-
-function card19() {
-
-$('#card19').addClass('animated zoomIn');  
-    
-document.getElementById("card19").src = heros[18];    
-}
-
-function card20() {
-
-$('#card20').addClass('animated zoomIn');  
-    
-document.getElementById("card20").src = heros[19];    
-}
-
-function card21() {
-$('#card21').addClass('animated zoomIn');  
-    
-document.getElementById("card21").src = heros[20];    
-}
-
-function card22() {
-$('#card22').addClass('animated zoomIn');  
-    
-document.getElementById("card22").src = heros[21];    
-}
-
-function card23() {
-$('#card23').addClass('animated zoomIn');  
-    
-document.getElementById("card23").src = heros[22];    
-}
-
-
-function card24() {
-    $('#card24').addClass('animated zoomIn');  
-    
-    document.getElementById("card24").src = heros[23];    
-}
-    
-    
-}
-    
-
-document.getElementsByTagName("BODY")[0].onresize = function() {resizeSmall()};
+document.getElementsByTagName("BODY")[0].onresize = function () {
+    resizeSmall()
+};
 
 function resizeSmall() {
-    
-  if (window.innerWidth <992 ) {
+
+    if (window.innerWidth < 992) {
         $("#5").remove();
         $("#6").remove();
         $("#11").remove();
@@ -346,30 +353,18 @@ function resizeSmall() {
         $("#18").remove();
         $("#23").remove();
         $("#24").remove();
-
-}
-    
-    
-else if( window.innerWidth > 992 && i == 1) {
-    
-      
-        alert("Reset the game for a full deck of cards")	
-
         
+        i=1;
+
+    } else if (window.innerWidth > 992 && i == 1) {
+
+
+        alert("Reset the game for a full deck of cards")
+
+
     }
-    
+
 }
-
-
-
-var hero1 = hero.sort(function(a, b){
-   return a < b? 1 : 0;
-
-    alert(heros);
-});
-
-
-
 /*
 
 
